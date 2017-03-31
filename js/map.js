bd81c30bb288a0effa9952ea0900498e8062889f
jhubz <jhubz@list.ru>
2017-03-31 18:40:23 +0300

    Сделал функцию для задания заголовков для объявлений, адрес теперь прописывается как координаты, сделал функцию для задания пути к фото Аватара, сделал шаблоны для объявления и контейнера диалога, сделал словарь соответвтий типа сдаваемого объекта, отредактировал вид pin'ов

--- js/map.js
+++ js/map.js
@@ -16,7 +16,7 @@ function getRandomArray(array) {
   var i;
 
   for (i = 0; i < array.length; i++) {
-    if (Math.random() >= 0.5) {
+    if (getRandomIntNumber(0, 1) >= 0.5) {
       randomArray.push(array[i]);
     }
   }
@@ -24,47 +24,66 @@ function getRandomArray(array) {
   return randomArray;
 }
 
+// ПОЛУЧЕНИЕ ПУТИ К ФАЙЛУ АВАТАРКИ
+function getAvatarPath(number) {
+  number++;
+  var avatarPath = 'img/avatars/user0' + number + '.png';
+
+  return avatarPath;
+}
+
 // СОЗДАНИЕ ОБЪЯВЛЕНИЯ
 function generatePin(number) {
-  var types = ['flat', 'house', 'bungalo'];
-  var times = ['12:00', '13:00', '14:00'];
-  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var title = 'Заголовок';
-  var address = 'Адрес';
-  var minPrice = 1000;
-  var maxPrice = 1000000;
-  var minRooms = 1;
-  var maxRooms = 5;
-  var minGuests = 1;
-  var maxGuests = 10;
-
   var minX = 300;
   var maxX = 900;
   var minY = 100;
   var maxY = 500;
+  var locationX = getRandomIntNumber(minX, maxX);
+  var locationY = getRandomIntNumber(minY, maxY);
+
+  var address = locationX + ', ' + locationY;
+
+  var minPrice = 1000;
+  var maxPrice = 1000000;
+  var price = getRandomIntNumber(minPrice, maxPrice);
+
+  var types = ['flat', 'house', 'bungalo'];
+  var type = types[getRandomIntNumber(0, types.length - 1)];
+
+  var minRooms = 1;
+  var maxRooms = 5;
+  var rooms = getRandomIntNumber(minRooms, maxRooms);
+
+  var guests = getRandomIntNumber(1, 30);
+
+  var times = ['12:00', '13:00', '14:00'];
+  var checkin = times[getRandomIntNumber(0, times.length - 1)];
+  var checkout = times[getRandomIntNumber(0, times.length - 1)];
+
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
   var pin = {
     'author': {
-      'avatar': 'img/avatars/user0' + (number + 1) + '.png'
+      'avatar': getAvatarPath(number)
     },
 
     'offer': {
-      'title': title,
+      'title': '',
       'address': address,
-      'price': getRandomIntNumber(minPrice, maxPrice),
-      'type': types[getRandomIntNumber(0, types.length - 1)],
-      'rooms': getRandomIntNumber(minRooms, maxRooms),
-      'guests': getRandomIntNumber(minGuests, maxGuests),
-      'checkin': times[getRandomIntNumber(0, times.length - 1)],
-      'checkout': times[getRandomIntNumber(0, times.length - 1)],
+      'price': price,
+      'type': type,
+      'rooms': rooms,
+      'guests': guests,
+      'checkin': checkin,
+      'checkout': checkout,
       'features': getRandomArray(features),
       'description': '',
       'photos': ''
     },
 
     'location': {
-      'x': getRandomIntNumber(minX, maxX),
-      'y': getRandomIntNumber(minY, maxY)
+      'x': locationX,
+      'y': locationY
     }
   };
 
@@ -75,6 +94,7 @@ function generatePin(number) {
 function generatePins() {
   var pins = [];
   var pinsCount = 8;
+
   var i;
 
   for (i = 0; i < pinsCount; i++) {
@@ -84,25 +104,46 @@ function generatePins() {
   return pins;
 }
 
+// ПРИСВОЕНИЕ TITLE ДЛЯ ОБЪЯВЛЕНИЙ
+function setPinTitles(array) {
+  var titles = [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостиприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+  ];
+  var title = '';
+
+  var randomIndex;
+  var i;
+
+  for (i = 0; i < array.length; i++) {
+    randomIndex = getRandomIntNumber(0, titles.length - 1);
+    title = titles.splice(randomIndex, 1)[0];
+    array[i].offer.title = title;
+  }
+}
+
 // СОЗДАНИЕ РАЗМЕТКИ ОБЪЯВЛЕНИЯ
 function createPinMark(x, y, avatarPath) {
+  var pinTemplate = document.querySelector('#pin-template');
+  var pinElement = pinTemplate.content.cloneNode(true);
+
+  var pin = pinElement.querySelector('.pin');
+  var pinImage = pin.querySelector('img');
+
   var pinWidth = 56;
   var pinHeight = 75;
-  var rootNodeClass = 'pin';
-  var rootNodeStyle = 'left: ' + (x - pinWidth / 2) + 'px; top: ' + (y - pinHeight) + 'px';
-  var pinMark;
-  var pinImageMark;
+  var pinStyle = 'left: ' + (x - pinWidth / 2) + 'px; top: ' + (y - pinHeight) + 'px';
 
-  pinMark = document.createElement('div');
-  pinMark.classList.add(rootNodeClass);
-  pinMark.setAttribute('style', rootNodeStyle);
+  pin.setAttribute('style', pinStyle);
+  pinImage.setAttribute('src', avatarPath);
 
-  pinImageMark = document.createElement('img');
-  pinImageMark.setAttribute('src', avatarPath);
-
-  pinMark.appendChild(pinImageMark);
-
-  return pinMark.outerHTML;
+  return pinElement;
 }
 
 // СОЗДАНИЕ МАССИВА РАЗМЕТОК ДЛЯ ОБЪЯВЛЕНИЙ
@@ -116,22 +157,27 @@ function createPinMarks(array) {
     pinMark = createPinMark(array[i].location.x, array[i].location.y, array[i].author.avatar);
     pinMarks.push(pinMark);
   }
+
   return pinMarks;
 }
 
 // ДОБАВЛЕНИЕ РАЗМЕТКИ ОБЪЯВЛЕНИЙ НА СТРАНИЦУ
 function addPinMarksOnPage(array) {
+  var fragment = document.createDocumentFragment();
+
   var i;
 
   for (i = 0; i < array.length; i++) {
-    pinMap.insertAdjacentHTML('beforeEnd', array[i]);
+    fragment.appendChild(array[i]);
   }
+
+  pinMap.appendChild(fragment);
 }
 
 // СОЗДАНИЕ МАССИВА РАЗМЕТОК УДОБСТВ
 function createFeaturesMark(pin) {
   var featureElement;
-  var featuresFragment = document.createDocumentFragment();
+  var fragment = document.createDocumentFragment();
 
   var i;
 
@@ -141,69 +187,39 @@ function createFeaturesMark(pin) {
     featureElement.classList.add('feature__image');
     featureElement.classList.add('feature__image--' + pin.offer.features[i]);
 
-    featuresFragment.appendChild(featureElement);
+    fragment.appendChild(featureElement);
   }
 
-  return featuresFragment;
+  return fragment;
 }
 
 // СОЗДАНИЕ РАЗМЕТКИ КОНТЕЙНЕРА ДЛЯ ДИАЛОГА
 function createDialogContainer(pin) {
-  var dialogContainer = document.createElement('div');
+  var dialogContainerTemplate = document.querySelector('#dialog-container-template');
+  var dialogContainerElement = dialogContainerTemplate.content.cloneNode(true);
 
-  var dialogTitle = document.createElement('div');
-  var dialogTitleImage = document.createElement('img');
-  var dialogTitleImageAlt = 'Avatar';
-  var dialogTitleImageWidth = '70';
-  var dialogTitleImageHeight = '70';
-
-  var dialogContainerClose = document.createElement('a');
-  var dialogContainerCloseHref = '#';
-  var dialogContainerCloseClass = 'dialog__close';
-
-  var dialogContainerCloseImage = document.createElement('img');
-  var dialogContainerCloseImageSrc = 'img/close.svg';
-  var dialogContainerCloseImageAlt = 'close';
-  var dialogContainerCloseImageWidth = '22';
-  var dialogContainerCloseImageHeight = '22';
-
-  dialogContainer.classList.add('dialog');
-
-  dialogTitle.classList.add('dialog__title');
+  var dialogTitleImage = dialogContainerElement.querySelector('.dialog-title img');
 
   dialogTitleImage.setAttribute('src', pin.author.avatar);
-  dialogTitleImage.setAttribute('alt', dialogTitleImageAlt);
-  dialogTitleImage.setAttribute('width', dialogTitleImageWidth);
-  dialogTitleImage.setAttribute('height', dialogTitleImageHeight);
 
-  dialogContainerClose.setAttribute('href', dialogContainerCloseHref);
-  dialogContainerClose.setAttribute('class', dialogContainerCloseClass);
-
-  dialogContainerCloseImage.setAttribute('src', dialogContainerCloseImageSrc);
-  dialogContainerCloseImage.setAttribute('alt', dialogContainerCloseImageAlt);
-  dialogContainerCloseImage.setAttribute('width', dialogContainerCloseImageWidth);
-  dialogContainerCloseImage.setAttribute('height', dialogContainerCloseImageHeight);
-
-  dialogContainerClose.appendChild(dialogContainerCloseImage);
-
-  dialogTitle.appendChild(dialogTitleImage);
-  dialogTitle.appendChild(dialogContainerClose);
-
-  dialogContainer.appendChild(dialogTitle);
-
-  return dialogContainer;
+  return dialogContainerElement;
 }
 
 // ПОЛУЧЕНИЕ ЗНАЧЕНИЯ ТИПА СДАВАЕМОГО ОБЪЕКТА
 function getOfferTypeValue(pin) {
-  var value = '';
+  var offerTypes = {
+    'flat' : 'Квартира',
+    'house' : 'Дом',
+    'bungalo' : 'Бунгало'
+  };
 
-  if (pin.offer.type === 'flat') {
-    value = 'Квартира';
-  } else if (pin.offer.type === 'house') {
-    value = 'Дом';
-  } else if (pin.offer.type === 'bungalo') {
-    value = 'Бунгало';
+  var value = '';
+  var key;
+
+  for (key in offerTypes) {
+    if (pin.offer.type === key) {
+      value = offerTypes[key];
+    }
   }
 
   return value;
@@ -212,10 +228,13 @@ function getOfferTypeValue(pin) {
 // ДОБАВЛЕНИЕ ДИАЛОГА НА СТРАНИЦУ
 function addDialogOnPage(pin) {
   var tokyo = document.querySelector('.tokyo');
+
+  var dialogContainerElement = createDialogContainer(pin);
+  var dialogContainer = dialogContainerElement.querySelector('.dialog');
+
   var offerDialog = tokyo.querySelector('#offer-dialog');
   var offerDialogTemplate = document.querySelector('#lodge-template');
   var dialogElement = offerDialogTemplate.content.cloneNode(true);
-  var dialogContainer = createDialogContainer(pin);
 
   var title = dialogElement.querySelector('.lodge__title');
   var address = dialogElement.querySelector('.lodge__address');
@@ -237,11 +256,12 @@ function addDialogOnPage(pin) {
 
   dialogContainer.appendChild(dialogElement);
 
-  tokyo.replaceChild(dialogContainer, offerDialog);
+  tokyo.replaceChild(dialogContainerElement, offerDialog);
 }
 
 function generateMap() {
   var pins = generatePins();
+  setPinTitles(pins);
   var pinMarks = createPinMarks(pins);
 
   addPinMarksOnPage(pinMarks);
